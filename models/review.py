c78e6770ece2324653518d7012ad309949471d41
Daniel-bee <daniel123_86@live.com>
2022-01-13 18:08:42 +0300

    review

--- models/review.py
+++ models/review.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+""" Review module for the HBNB project """
+from os import getenv
+import sqlalchemy
+import models
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -9,15 +12,17 @@ from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """Represents a review for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Reviews.
-        text (sqlalchemy String): The review description.
-        place_id (sqlalchemy String): The review's place id.
-        user_id (sqlalchemy String): The review's user id.
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """ Review classto store review information """
+    if models.storage_t == 'db':
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
