ed1872503f026f7288e161dc4f2b3ca39e82883b
Daniel-bee <daniel123_86@live.com>
2022-01-15 16:06:40 +0300

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello World!" > /data/web_static/releases/test/index.html
+echo "ALX + Holberton" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
